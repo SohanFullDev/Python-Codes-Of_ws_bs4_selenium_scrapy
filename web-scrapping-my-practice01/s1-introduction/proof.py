0f48820e4050678e8c616e0ca9da494d997f214e
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-07 14:17:56 +0600

    1.2_python basics for web scrapping#1*4

--- web-scrapping-my-practice01/s1-introduction/proof.py
+++ web-scrapping-my-practice01/s1-introduction/proof.py
@@ -1,3 +1,10 @@
 states = ["California","Texas","Florida","New York"];
 
-print(states[0]);
+#print(states[3]);
+#print(states[-1]);
+#print(states[-2]);
+#print(states[-3]);
+#print(states[-4])
+
+for state in states:
+    print(state)
