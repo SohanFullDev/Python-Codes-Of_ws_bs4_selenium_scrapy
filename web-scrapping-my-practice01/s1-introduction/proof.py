c52ab53b15614cc7588afaa2b2906a60c0226ff0
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-07 15:11:35 +0600

    1.6_csv file creating_python basics for web scrapping#1*4

--- web-scrapping-my-practice01/s1-introduction/proof.py
+++ web-scrapping-my-practice01/s1-introduction/proof.py
@@ -6,7 +6,9 @@ population = [396123394, 29730311, 21944577, 19299981];
 dict_states = {'States': states, 'Population': population};
 
 df_states = pd.DataFrame.from_dict(dict_states);
-print(df_states);
+#print(df_states);
+df_states.to_csv('states.csv');
+
 
 
 
