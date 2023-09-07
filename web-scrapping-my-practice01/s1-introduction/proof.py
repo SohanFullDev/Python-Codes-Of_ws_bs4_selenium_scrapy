e7fe8bf660cf61d266468d713906db3be34833ad
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-07 15:16:39 +0600

    1.7_index_false_python basics for web scrapping#1*4

--- web-scrapping-my-practice01/s1-introduction/proof.py
+++ web-scrapping-my-practice01/s1-introduction/proof.py
@@ -7,9 +7,9 @@ dict_states = {'States': states, 'Population': population};
 
 df_states = pd.DataFrame.from_dict(dict_states);
 #print(df_states);
-df_states.to_csv('states.csv');
+df_states.to_csv('states.csv', index=False);
 
-
+git 
 
 
 
