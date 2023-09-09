8ef16941c8bc6ec7f1c40be498f4f0dbe329afe5
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-09 13:40:04 +0600

    2.6_how to scrape a single page_part4#2*12

--- web-scrapping-my-practice01/s2-introduction to bs4/beautifulsoup1.py
+++ web-scrapping-my-practice01/s2-introduction to bs4/beautifulsoup1.py
@@ -12,8 +12,6 @@ box = soup.find('article', class_='main-article')
 title = box.find('h1').get_text()
 
 transcript = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
-print(title)
-print(transcript)
 
-
-
+with open(f'{title}.txt', 'w',encoding="utf-8") as file:
+    file.write(transcript)
